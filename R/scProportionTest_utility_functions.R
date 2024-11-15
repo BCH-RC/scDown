46e5dd1d43c57d99f8ccf31d1111a758adba9694
Maryam Labaf <40154433+MaryamLabaf@users.noreply.github.com>
2024-11-15 15:25:55 -0500

    Update scProportionTest_utility_functions.R

--- R/scProportionTest_utility_functions.R
+++ R/scProportionTest_utility_functions.R
@@ -8,14 +8,14 @@ library(dplyr)
 library(tidyverse)
 
 #' Create result directories for scProportionTest figures
-#' @param dir_scproportion Folder path for scProportionTest figures
+#' @param output_dir Folder path for scProportionTest figures
 #' @return NULL
 #' @noRd
 
-create_dir <- function(dir_scproportion) {
+create_dir <- function(output_dir) {
 
-  subdirectories <- c(file.path(dir_scproportion, "scproportion", "images"),
-                      file.path(dir_scproportion, "scproportion", "results"))
+  subdirectories <- c(file.path(output_dir, "scproportion", "images"),
+                      file.path(output_dir, "scproportion", "results"))
   
   # Create each directory if it doesn't exist
   for (dir.i in subdirectories) {
@@ -23,20 +23,20 @@ create_dir <- function(dir_scproportion) {
   }
   
   # Return the path to the main directory for further use if needed
-  return(file.path(dir_scproportion, "scproportion"))
+  return(file.path(output_dir, "scproportion"))
 }
 
 #' Generate plot for each comparision
 #' @param prop_test.i scProportion object
 #' @param output.format  The format of output figure
 #' @param comparisons_condition table of all the pairwise comparison
-#' @param dir_scproportion path to save the figures 
+#' @param output_dir path to save the figures 
 #' @param cluster_col the column name in the meta, cluster or celltype
 #' @param i index of the comparison condition
 #' @return NULL saves comparison figures in the specified directory.
 #' @noRd
 
-generate_figure <- function(prop_test.i, output.format="png",comparisons_condition, dir_scproportion=".", cluster_col, i) {
+generate_figure <- function(prop_test.i, output.format="png",comparisons_condition, output_dir=".", cluster_col, i) {
   p <- permutation_plot(prop_test.i) +
     theme_bw(base_size = 12) +
     labs(title = paste0(comparisons_condition[i, 1], " vs ", comparisons_condition[i, 2]),
@@ -49,7 +49,7 @@ generate_figure <- function(prop_test.i, output.format="png",comparisons_conditi
   output_format <- match.arg(output.format, choices = c("png", "pdf", "jpeg"))
   file_extension <- switch(output_format, png = "png", pdf = "pdf", jpeg = "jpg")
   
-  output_path <- paste0(dir_scproportion, "scproportion/images/scProportiontest_",
+  output_path <- paste0(output_dir, "scproportion/images/scProportiontest_",
                         comparisons_condition[i, 1], "vs", comparisons_condition[i, 2], ".", file_extension)
   if (output_format == "png") {
     png(output_path, width = 2000, height = 1250, res = 350)
@@ -65,14 +65,14 @@ generate_figure <- function(prop_test.i, output.format="png",comparisons_conditi
 #' Generate table of stat results for each comparision
 #' @param prop_test.i scProportion object
 #' @param comparisons_condition table of comparisions
-#' @param dir_scproportion directory to save the results table
+#' @param output_dir directory to save the results table
 #' @param i index of the comparison condition
 #' @return NULL saves comparison figures in the specified directory.
 #' @noRd
 
-stat_res <- function(prop_test.i,comparisons_condition, dir_scproportion, i){
+stat_res <- function(prop_test.i,comparisons_condition, output_dir, i){
   res_tab <- prop_test.i@results %>% as.data.frame()
-  output_path <- paste0(dir_scproportion, "scproportion/results/scProportiontest_",
+  output_path <- paste0(output_dir, "scproportion/results/scProportiontest_",
                         comparisons_condition[i, 1], "vs", comparisons_condition[i, 2], ".csv")
   write.csv(res_tab, output_path, row.names = FALSE)
 }
