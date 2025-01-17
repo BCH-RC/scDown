8239b0d117fb172e32f29ba6dc70431d8e31a806
qianqianshao <qzm100@gmail.com>
2025-01-17 12:37:55 -0500

    Use environment.yml to create conda environment

--- R/zzz.R
+++ R/zzz.R
@@ -3,41 +3,35 @@
 #' install required python dependencies for scvelo_workflow.py
 .onLoad <- function(libname, pkgname) {
 
-  library(reticulate)
-
   # Check if Python is available without initializing
   reticulate::py_available(initialize = FALSE)
 
   # Set the path to the Miniconda installation if Python is not available
-  miniconda_path <- "~/.local/share/r-miniconda"
-  # Check if Miniconda is installed at the specified path
+  conda_path <- reticulate::conda_binary()
+  # Check if Miniconda is installed 
   if (!reticulate::py_available(initialize = FALSE)) {
-    if (!file.exists(miniconda_path)) {
-      # If Miniconda is not installed, install it at specific path
+    if (is.null(conda_path)) {
+      # If Miniconda is not installed, install it
+      miniconda_path <- "~/.local/share/r-miniconda"
       reticulate::install_miniconda()
     } 
-    # use the minoconda at specific path
-    reticulate::use_miniconda(miniconda_path, required = TRUE)
   }
+  conda_path <- reticulate::conda_binary()
+  conda_path2=gsub("/conda$","",conda_path)
 
   env_name <- "scvelo"
+  env_file <- "inst/env/environment.yml"
+
   env_exists <- env_name %in% reticulate::conda_list()$name 
   # # Check if the conda environment exists
-  if (!env_exists) {
-    reticulate::conda_create(env_name)
+  if (!env_exists) {  
+    system(paste0(conda_path2, "/conda env create -f environment.yml"))
+    #reticulate::conda_create(env_name, envfile = env_file, dependencies = c("python=3.10.9"))
   }
   # Use the conda environment
-  reticulate::use_condaenv(env_name, required = TRUE)
-
-  packages=c("scvelo", "anndata", "loompy", "matplotlib", "numba", "numpy", "pandas", "scanpy", "scikit-learn", "scipy", "scvi-tools", "umap-learn")
-  # Install the required packages 
-  for (pkg in packages) {
-    installed_packages <- py_module_available(pkg)
-    if (!installed_packages) {
-      cat("Installing Conda package:", pkg, "\n")
-      reticulate::conda_install(envname = env_name, packages = pkg, channel = "conda-forge")
-    }
-  }
+  print(conda_path2)
+  print(system(paste(conda_path, "info --envs")))
+  reticulate::use_condaenv(env_name, conda = "auto", required = TRUE)
 
 
 }
