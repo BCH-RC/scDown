6e962a4c94606ffcfb266f271dbe9bc1b06e3fe1
Qianyi Ma <qzm100@gmail.com>
2025-03-06 12:16:05 -0500

    Update h5adToSeurat to add spliced and unspliced layers if available in h5ad in utility_functions.R

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,46 +1,72 @@
-#' Read in a h5ad file and transfer the AnnData object into a Seurat object.
+#' Read in a h5ad file and convert to a Seurat object.
 #'
-#' @description This function uses the library(anndata) to transfer the normalized counts, metadata,
-#' and lower dimensional embeddings (pca and umap) from a h5ad file into a Seurat object.
-#' More data can be transferred between the two formats, and this function can be expanded
-#' to accommodate that accordingly.
-#'
+#' @description This function uses the library(zellkonverter) to convert the normalized counts, metadata,
+#' lower dimensional embeddings (pca and umap), and spliced and unspliced counts if available from a h5ad 
+#' file to a Seurat object.
 #'
 #' @param h5ad_file character string of full path to the h5ad file.
+#' @param annotation_column character variable specifying the metdata column name of cell type annotations. 
 #' @return a Seurat object.
 #'
 #' @export
-h5adToSeurat <- function(h5ad_file, annotation_column){
-  miniconda_path <- "~/.local/share/r-miniconda"
-  # Check if Miniconda is already installed at the specified path
-  if (!file.exists(miniconda_path)) {
-    reticulate::install_miniconda()
-  }
+#'
+h5adToSeurat <- function(h5ad_file, annotation_column=NULL){
 
-  library(anndata)
-  print("Convert from AnnData to Seurat...")
-  adata <- read_h5ad(h5ad_file)
-  # here normalized counts are used to create the Seurat object, raw counts are typically in adata$raw$X
-  data <- CreateSeuratObject(counts = t(adata$X), meta.data = adata$obs)
-
-  # transfer lower dimensional embeddings
-  if ('X_pca' %in% names(adata$obsm)){
-    umap_data <- adata$obsm$X_pca
-    data <- AddDimReduc(data, reduction = umap_data, key = "pca")
-  }
-  if ('X_umap' %in% names(adata$obsm)){
-    umap_data <- adata$obsm$X_umap
-    data <- AddDimReduc(data, reduction = umap_data, key = "umap")
-  }
-  print("AnnData object successfully converted.")
-
-  # save transferred Seurat object
+  # Convert .h5ad to Seurat object
+  library(zellkonverter)
+  # Read the .h5ad file
+  ad <- readH5AD(h5ad_file)
+  ad
   file_sub <- sub("\\.h5ad$", "", h5ad_file)
-  saveRDS(data, file=paste0(file_sub,"_h5adTransferred.rds",sep=""))
 
-  return(data)
+  # Convert the main count matrix of h5ad to RNA assay of a Seurat object
+  assays <- names(ad@assays)
+  default_assay_name <- ifelse("X" %in% assays, "X", assays[1])
+  X <- as.Seurat(ad, counts = default_assay_name, data = NULL)
+  X@assays[["RNA"]]<-CreateAssayObject(counts = X@assays[["originalexp"]]@counts)
+  X@assays[["RNA"]]@key<-"rna_"
+
+  # Convert spliced and unspliced layers if available in original h5ad
+  if("spliced" %in% names(ad@assays)){
+    spliced_mat <- as.matrix(ad@assays@data$spliced)
+    rownames(spliced_mat) <- rownames(ad)
+    colnames(spliced_mat) <- colnames(ad)  
+    X@assays[["spliced"]]<-CreateAssayObject(counts = spliced_mat)
+    X@assays[["spliced"]]@key<-"spliced_"
+    file_sub<-paste0(file_sub,"_spliced")
+  }
+  if("unspliced" %in% names(ad@assays)){
+    unspliced_mat <- as.matrix(ad@assays@data$unspliced)
+    rownames(unspliced_mat) <- rownames(ad)
+    colnames(unspliced_mat) <- colnames(ad)  
+    X@assays[["unspliced"]]<-CreateAssayObject(counts = unspliced_mat)
+    X@assays[["unspliced"]]@key<-"unspliced_"
+    file_sub<-paste0(file_sub,"_unspliced")
+  }
+
+  # Set Default Assay to RNA
+  DefaultAssay(X) <- "RNA"
+
+  # Convert lower dimensional embeddings (pca and umap) if available
+  if ('X_pca' %in% names(X@reductions)){
+    pca_coords<-X@reductions[["X_pca"]]@cell.embeddings
+    X@reductions[["pca"]] <- CreateDimReducObject(embeddings = pca_coords, key = "PCA_", assay = "RNA")
+  }
+  if ('X_umap' %in% names(X@reductions)){
+    umap_coords<-X@reductions[["X_umap"]]@cell.embeddings
+    X@reductions[["umap"]] <- CreateDimReducObject(embeddings = umap_coords, key = "UMAP_", assay = "RNA")
+  }
+
+  # use cell type annotation column as identity if provided
+  if (!is.null(annotation_column) && (annotation_column %in% colnames(X@meta.data))) {
+    Idents(X)<-X[[annotation_column]]
+  } 
+
+  # save converted Seurat object
+  saveRDS(X, file=paste0(file_sub,".rds"))
+
+  return(X)
 }
-
 
 #' Check the required input objects for all the functions.
 #'
